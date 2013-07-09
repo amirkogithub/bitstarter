d8783270696d646454541fb83d2a84fdee5ee733
Amir Shrestha <amir.shrestha.usa@gmail.com>
2013-07-09 00:49:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from index.html');
+  //response.send('Hello World from index.html');
+  var param = fs.readFileSync('./index.html','utf8');
+  response.send(txt.toString());
 });
 
 var port = process.env.PORT || 5000;
